f783eec4a5d5a516cd4b33044c6a868bff6f5baa
John Hupperts <39680353+treatmesubj@users.noreply.github.com>
2020-05-26 19:03:39 -0500

    android linux support

--- ght.py
+++ ght.py
@@ -1,15 +1,35 @@
 import requests
-from requests_html import HTML
+import platform
 
-html = HTML(html=requests.get("https://github.com/trending/python?since=daily").text)
+if platform.system() == 'Windows':
+	from requests_html import HTML
 
-for proj in html.find("article"):
-	title = proj.find("h1 a", first=True)
-	desc = proj.find("p", first=True)
-	print(f"~~~{title.text}~~~", " {")
-	print(f"	https://github.com{title.attrs['href']}")
-	try:
-		print(f"	{desc.text}")
-	except AttributeError:
-		pass
-	print("}", end="\n\n")
+	html = HTML(html=requests.get("https://github.com/trending/python?since=daily").text)
+
+	for proj in html.find("article"):
+		title = proj.find("h1 a", first=True)
+		desc = proj.find("p", first=True)
+		print(f"~~~{title.text}~~~", " {")
+		print(f"	https://github.com{title.attrs['href']}")
+		try:
+			print(f"	{desc.text}")
+		except AttributeError:
+			pass
+		print("}", end="\n\n")
+
+if platform.system() == 'Linux':
+	from bs4 import BeautifulSoup
+
+	html = requests.get("https://github.com/trending/python?since=daily").text
+	soup = BeautifulSoup(html, 'html.parser')
+
+	for proj in soup.select("article"):
+		title = proj.select_one("h1 a")
+		desc = proj.select_one("p")
+		print(f"{[x.strip() for x in title.text.split('/')]}")
+		print(f"https://github.com{title.attrs['href']}")
+		try:
+			print(f"{desc.text.strip()}")
+		except AttributeError:
+			pass
+		print("\n")
