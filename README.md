205587bde3d8e281855c61c7056ccb5b87a8c3ad
treatmesubj <jrock4503@hotmail.com>
2021-09-10 17:02:13 -0500

    hacker news

--- README.md
+++ README.md
@@ -1,16 +1,21 @@
-# N-Gate
-Ironically read the n-gate's latest post, most likely an annotated digest of the top "Hacker" "News" posts, on the command line
+# Hacker News
+Fetch the front page of Hacker News
 
-![](attachments/Pasted%20image%2020210507120255.png)
+![](./attachments/hacker_news.jpg)
+
+# N-Gate
+Ironically read the n-gate's latest post, most likely an annotated digest of the top "Hacker" "News" posts
+
+![](./attachments/ngate.png)
 
 # /r/whatstheword
 Fetches /r/whatstheword/top/ and allows you to choose a post to read its comments
 
 Usage: `python wtw.py [1hr | 24hrs | week]`
 
-![](attachments/wtw_demo.jpg)
+![](./attachments/wtw_demo.jpg)
 
 # Github Trending
 Gathers the day's trending Python repositories on Github and displays brief info about them
 
-![](attachments/Pasted%20image%2020210507120327.png)
+![](./attachments/webshit.png)
---
+++ hacker_news.py
@@ -0,0 +1,42 @@
+import requests
+from bs4 import BeautifulSoup
+from rich import pretty, print;pretty.install();
+
+res = requests.get('https://news.ycombinator.com')
+soup = BeautifulSoup(res.text, 'html.parser')
+
+post_elems = soup.select('.storylink')
+subtext_elems = soup.select('.subtext')
+
+
+def sort_news_by_votes(news_info):
+    return sorted(news_info, key=lambda k:k['points'], reverse=True)
+
+
+def get_news_info(post_elems, subtext_elems):
+    news_info = []
+
+    for index, elem in enumerate(post_elems):
+        title = post_elems[index].getText()
+        story_link = post_elems[index].get('href', None)
+        try:
+            points = int(subtext_elems[index].select_one('.score').getText().strip(' points'))
+        except AttributeError:
+            points = None
+        try:
+            comments_link = subtext_elems[index].select_one("a[href*='item']").get('href', None)
+        except AttributeError:
+            comments_link = None
+
+        if points:
+            news_info.append({'title': title, 'story_link': story_link, 'points': points, 'comments_link': f"https://news.ycombinator.com/{comments_link}"})
+
+    return sort_news_by_votes(news_info)
+
+
+if __name__ == "__main__":
+    news_info = get_news_info(post_elems, subtext_elems)
+    for post in news_info:
+        print(f"[{post['points']}]: {post['title']}")
+        print(f"story: {post['story_link']}")
+        print(f"comments: {post['comments_link']}\n")
